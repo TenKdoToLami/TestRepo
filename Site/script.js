bd8727de929b349ab398785147bf11d7d7b37f36
Jan Bartos <barty@pop-os.localdomain>
2023-12-21 22:37:13 +0100

    Initial setup

---
+++ Site/script.js
@@ -0,0 +1,20 @@
+function changeContent(buttonNumber) {
+    var contentSection = document.getElementById('content');
+
+    switch (buttonNumber) {
+        case 1:
+            contentSection.innerHTML = '<p>About Me content goes here.</p>';
+            break;
+        case 2:
+            contentSection.innerHTML = '<p>Currents Project content goes here.</p>';
+            break;
+        case 3:
+            contentSection.innerHTML = '<p>Documentation content goes here.</p>';
+            break;
+        case 4:
+            contentSection.innerHTML = '<p>Demos content goes here.</p>';
+            break;
+        default:
+            contentSection.innerHTML = '<p>Default content goes here.</p>';
+    }
+}
